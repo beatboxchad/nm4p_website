7b8f3f1ef67248d2622e5380c68faf22fa8f29e1
Chad Cassady <chad.cassady@gmail.com>
2024-09-30 16:18:01 -0500

    python script that generates token, auth needs fleshing out

---
+++ tools/generate_token.py
@@ -0,0 +1,21 @@
+import json
+import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/calendar',
+          'https://www.googleapis.com/auth/drive.readonly'
+          ]
+
+def main():
+    # Load the OAuth credentials file
+    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+    creds = flow.run_local_server(port=42669)
+
+    # Save the credentials for future use
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+    print("Access token and refresh token generated.")
+
+if __name__ == '__main__':
+    main()
