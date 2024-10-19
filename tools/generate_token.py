9fe9aafa8dba80e6a3c15058be67a33a77b91a1c
Chad Cassady <chad.cassady@gmail.com>
2024-10-19 17:54:57 -0500

    format tools/generate_token.py with black

--- tools/generate_token.py
+++ tools/generate_token.py
@@ -2,20 +2,23 @@ import json
 import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-SCOPES = ['https://www.googleapis.com/auth/calendar',
-          'https://www.googleapis.com/auth/drive.readonly'
-          ]
+SCOPES = [
+    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/drive.readonly",
+]
+
 
 def main():
     # Load the OAuth credentials file
-    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
     creds = flow.run_local_server(port=42669)
 
     # Save the credentials for future use
-    with open('token.json', 'w') as token:
+    with open("token.json", "w") as token:
         token.write(creds.to_json())
 
     print("Access token and refresh token generated.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
